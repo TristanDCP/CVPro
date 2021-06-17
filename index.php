a8d4dd64b52696929bfa94feafe172bd6e4223ea
TristanDCP <tristandacostapinto@gmail.com>
2021-06-17 02:56:06 +0200

    V1

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Tristan Da Costa Pinto</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.3/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Tristan Da Costa Pinto</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="assets/img/profilepicture.jpg" alt="..." /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">A propos de moi</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Expériences passées</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Diplômes</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Compétences</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interêts</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Tristan
+                        <span class="text-primary">Da Costa Pinto</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Perdu au fin fond de la Normandie·
+                        <a href="mailto:tristandacostapinto@gmail.com">tristandacostapinto@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">Je suis très fort pour copier les sites des autres. Je serais capable de copier dans toute langues disponibles</p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="https://www.linkedin.com/in/tristan-da-costa-pinto-823b0618a/"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="https://github.com/TristanDaCostaPinto"><i class="fab fa-github"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Junior en développement web</h3>
+                            <div class="subheading mb-3">Renault</div>
+                            <p>J'ai eu l'occasion d'intégrer l'équipe FAST-IT de Renault et de compléter mes connaissances de développeur avec de l'angular et du symfony. Pendant un an, j'ai suivi quotidiennement les mêlées matinales et ait mené plusieurs projets seul pour Renault France ou d'autres usines externes (Cordoba)</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Octobre 2020 - Octobre 2021</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Diplômes</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Titre Professionnel « Développeur web et web mobile » RNCP 31114 BAC+2, Niveau 5</h3>
+                            <div class="subheading mb-3">La Manu - Le Havre</div>
+                            <div>Développement web</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">23 Mars 2020 - 23 Septembre 2020</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">LLCER - Licence Anglais - Option Japonais</h3>
+                            <div class="subheading mb-3">Université du Havre</div>
+                            <div>Apprentissage de l'anglais avancé et de la culture anglosaxonne</div>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Septembre 2016 - Juin 2020</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Compétences</h2>
+                    <div class="subheading mb-3">Langages et outils utilisés</div>
+                    <ul class="list-inline dev-icons">
+                        <li class="list-inline-item"><i class="fab fa-html5"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-css3-alt"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-js-square"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-php"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-angular"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-react"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-wordpress"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-npm"></i></li>
+                    </ul>
+                    <div class="subheading mb-3">Pratiques de travail : </div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Priorité au responsive design
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                           Test inter-navigateur & debug
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            Capable d'utiliser les méthodes agiles
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="fas fa-check"></i></span>
+                            S'intègre parfaitement aux équipes
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Interests-->
+            <section class="resume-section" id="interests">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Interêts</h2>
+                    <p>Autre que mon attrait particulier pour le développement web, ce que j'aime par dessus tout est apprendre. La capacité de l'humain d'étendre en permanence ses connaissances est fascinante pour moi</p>
+                    <p class="mb-0">Au delà de cela, j'apprécie particulièrement la musique, le café et les jeu vidéos</p>
+                </div>
+            </section>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
